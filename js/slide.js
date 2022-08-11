30c632fce0cf8b36801d68b92dffc5ce9b256647
Kauã <71743008+KLusvarghi@users.noreply.github.com>
2022-08-11 08:34:22 -0300

    configurações e index dos slides

--- js/slide.js
+++ js/slide.js
@@ -82,9 +82,50 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this)
   }
 
+
+
+  // slides config
+
+
+  // método para fazer um calculo para colcar o slide no centro da tela
+  slidePosition(slide) { // recebe a arraay de slide
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2; // assim, pegando a largura da tela inteira com o "wrapper", e subtraindo com o a lardura da tela em relação ao slide do slide especifico
+    return -(slide.offsetLeft - margin) //fazendo então a largura do slide em relção a tela menos a margin, colcondo o valor em negativo para que centralize e não vá mais para a esquerda
+
+  }
+
+  slidesConfig() {
+    // desestruturando os "slides" (que são as li) e colocando dentro de uma array, fazendo um map que a sua array será o que estiver dentro do 'return'
+    this.slideArray = [...this.slide.children].map( (element) => {
+      const position = this.slidePosition(element); // passando a array de slides como parametro, retornando o valor do elemento no centro
+      return { position,element } // como a propriedade tem o mesmo nome que o valor, não é necessario colocar o nome duas vezes
+    }); 
+  }
+
+
+  // método para saber o anterior e próximo slides
+  slidesIndexNav(index){
+    const last = this.slideArray.length - 1 //pegando o tamanho da mnha lsita de slides
+    this.index = { //sendo o index do slide anterior, do atual e do proximos
+      prev: index ? index -1 : undefined, // se o slide existir ele irá retornar "index-1", caso ele 0, ele irá retornar "undefined"
+      active: index,
+      next: index === last ? undefined : index + 1, //se o index for igual a ultima posição, ele terá o valor de "undefined", caso não ele terá valor index + 1
+    }
+  }
+
+
+  // método que irá chamar o "moveSlide", passando o slide para qual irá e passando tambem a posição que ele ficará
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index]; 
+    this.moveSlide(activeSlide.position)
+    this.slidesIndexNav(index)
+    this.dist.finalPosition = activeSlide.position // passando para a propriedade do objeto o valor final do slide
+  }
+
   init(){
     this.bindEvents()
     this.addSlideEvents()
+    this.slidesConfig()
     return this
   }
 }
