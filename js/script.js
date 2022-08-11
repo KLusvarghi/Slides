d13a4415259e9b7f09f54a4c7441230a0304cf00
Kauã <71743008+KLusvarghi@users.noreply.github.com>
2022-08-11 10:08:53 -0300

    navegação por botão atualizada

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-import Slide from './slide.js';
+import {Slidenav} from './slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new Slidenav('.slide', '.slide-wrapper');
 slide.init()
 
-slide.changeSlide(4)
-slide.activePrevSlide()
+
+slide.addArrow('.prev', '.next')
+
